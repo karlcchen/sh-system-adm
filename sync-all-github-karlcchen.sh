f69e9aed7b89171fef493d436879875a6ee2e3b2
Karl Chen <kchen@sonicwall.com>
2019-08-16 16:34:24 -0700

    add kchen repo

--- sync-all-github-karlcchen.sh
+++ sync-all-github-karlcchen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #
 cd
-~/sh-system-adm/git-pull.sh sh-glp2 sh-ubuntu-install sh-system-adm sh-uboot sh-soniccorex sh-bitbake sh-lpower etc bin
+~/sh-system-adm/git-pull.sh sh-glp2 sh-ubuntu-install sh-system-adm sh-uboot sh-soniccorex sh-bitbake sh-lpower etc bin kchen
 
