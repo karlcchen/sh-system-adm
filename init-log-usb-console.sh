e61490f6e70ff37effcc4ee5065af3c2b9c1b910
Karl Chen <kchen@sonicwall.com>
2019-09-12 02:19:12 -0700

    add init-log-usb-console.sh

---
+++ init-log-usb-console.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+#
+LOG_BASE_DIR="uc-log"
+LOG_BASE_FILE="uc"
+#
+mkdir -p ~/${LOG_BASE_DIR}/${LOG_BASE_FILE}0
+mkdir -p ~/${LOG_BASE_DIR}/${LOG_BASE_FILE}1
+mkdir -p ~/${LOG_BASE_DIR}/${LOG_BASE_FILE}2
+mkdir -p ~/${LOG_BASE_DIR}/${LOG_BASE_FILE}3
+mkdir -p ~/${LOG_BASE_DIR}/${LOG_BASE_FILE}4
+mkdir -p ~/${LOG_BASE_DIR}/${LOG_BASE_FILE}5
+mkdir -p ~/${LOG_BASE_DIR}/${LOG_BASE_FILE}6
+mkdir -p ~/${LOG_BASE_DIR}/${LOG_BASE_FILE}7
+mkdir -p ~/${LOG_BASE_DIR}/${LOG_BASE_FILE}8
+mkdir -p ~/${LOG_BASE_DIR}/${LOG_BASE_FILE}9
