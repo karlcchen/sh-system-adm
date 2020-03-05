ffb0e7ba0ebb26cd953b50a4e053e7fe0904160b
Karl Chen <kchen@sonicwall.com>
2020-03-05 00:20:27 -0800

    update

--- network-service-restart.sh
+++ network-service-restart.sh
@@ -1,3 +1,14 @@
+#wq old method 
 # sudo service networking restart
-sudo systemctl restart networking.service
+#
+
+# replaced with proper interface names  
+# sudo ifdown --force enp0s3 lo && ifup -a
+#
+
+sudo systemctl unmask networking
+sudo systemctl enable networking
+sudo systemctl restart networking
+
+# sudo systemctl restart networking.service
 ./network-service-status.sh
