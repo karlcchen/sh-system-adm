15f65d1d3b735a75a5674c0452a4fa4efeda4405
Karl Chen <kchen@sonicwall.com>
2020-05-13 10:39:15 -0700

    check error of screenrc file exist or not

--- usb-console.sh
+++ usb-console.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-sudo screen -c /etc/screenrc.d/usbcon-screenrc -d -m
+
+SCREENRC_FILE=""/etc/screenrc.d/usbcon-screenrc""
+
+if [ -f "${SCREENRC_FILE}" ] ; then 
+    sudo screen -c ${SCREENRC_FILE} -d -m
+else
+    printf "ERROR1: cannot find file: %s" "${SCREENRC_FILE}"
+    exit 1
+fi
