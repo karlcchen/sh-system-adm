929a7c7b81f2c82c9afb28032ed66cbebf8fb9ea
Karl Chen <kchen@sonicwall.com>
2020-06-25 12:18:52 -0700

    add ascii colcors in usb-console.sh

--- usb-console.sh
+++ usb-console.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+EXE_NAME=`realpath $0`
+EXE_DIR=`dirname ${EXE_NAME}`
+
+SCR_LS=`sudo screen -ls`
+if [ $? -eq 0 ] ; then 
+    asc yellow blink
+    printf "\n A sudo screen session is running:\n"
+    asc unblink red
+    printf "\n%s\n\n" "${SCR_LS}"
+    asc reset
+    exit 1 
+fi 
+
 SCREENRC_FILE=""/etc/screenrc.d/usbcon-screenrc""
 
 if [ -f "${SCREENRC_FILE}" ] ; then 
