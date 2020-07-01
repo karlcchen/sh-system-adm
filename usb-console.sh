75b0d25f1a9b833f09c0863a1c397ae34b6b1e16
Karl Chen <kchen@sonicwall.com>
2020-07-01 10:49:43 -0700

    add usb-console.sh detect called from sudo or not

--- usb-console.sh
+++ usb-console.sh
@@ -3,20 +3,31 @@
 EXE_NAME=`realpath $0`
 EXE_DIR=`dirname ${EXE_NAME}`
 
-SCR_LS=`sudo screen -ls`
+# use ~/bin as where asc is installed
+ASC_DIR="`realpath ~/bin`"
+
+WHOAMI=`whoami`
+
+if [ "${WHOAMI}" = "root" ] ; then 
+    SUDO_CMD=""  
+else
+    SUDO_CMD="sudo"
+fi
+
+SCR_LS=`${SUDO_CMD} screen -ls`
 if [ $? -eq 0 ] ; then 
-    asc yellow blink
+    ${ASC_DIR}/asc yellow blink
     printf "\n A sudo screen session is running:\n"
-    asc unblink red
+    ${ASC_DIR}/asc unblink red
     printf "\n%s\n\n" "${SCR_LS}"
-    asc reset
+    ${ASC_DIR}/asc reset
     exit 1 
 fi 
 
 SCREENRC_FILE=""/etc/screenrc.d/usbcon-screenrc""
 
 if [ -f "${SCREENRC_FILE}" ] ; then 
-    sudo screen -c ${SCREENRC_FILE} -d -m
+    ${SUDO_CMD} screen -c ${SCREENRC_FILE} -d -m
 else
     printf "ERROR1: cannot find file: %s" "${SCREENRC_FILE}"
     exit 1
