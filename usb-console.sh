e58c67c3340d65d774c32cbd113880102b6d06f6
Karl Chen <kchen@sonicwall.com>
2020-12-02 15:18:55 -0800

    update usb-console.sh

--- usb-console.sh
+++ usb-console.sh
@@ -24,40 +24,40 @@ fi
 # use ~/bin as where asc is installed
 ASC_DIR="`realpath ~/bin`"
 
+SCR_LS="`screen -ls`"
+if [[ $? -eq 0 ]]; then 
+    ${ASC_DIR}/asc yellow blink
+    printf '\nERROR-2: A GNU screen session already exist:\n'
+    ${ASC_DIR}/asc unblink red
+    printf '\n%s\n\n' "${SCR_LS}"
+    ${ASC_DIR}/asc reset
+    exit 2 
+fi 
+
 MY_GROUP="`id -Gn`"
 echo "${MY_GROUP}" | grep "${DEV_GROUP}" >/dev/null
 if [[ $? -ne 0 ]]; then 
-    printf '\nERROR-2: must belong to the same group \"%s\" as device \"%s\"\n' "${DEV_GROUP}" "${TEST_DEV}"
+    printf '\nERROR-3: must belong to the same group \"%s\" as device \"%s\"\n' "${DEV_GROUP}" "${TEST_DEV}"
     printf '%s Your Groups: %s\n' "$(whoami)" "${MY_GROUPS}"
-    exit 2
+    exit 3
 fi 
 
 # if /usr/bin/screen has sticky bit set, then 755 will be good
 # otherwise, need 777 
-sudo chmod 777 /run/screen
+sudo chmod 755 /run/screen
 if [[ $? -ne 0 ]]; then 
-    printf '\nERROR-3: chmod 755 %s failed!\n' "/run/screen"
-    exit 3
+    printf '\nERROR-4: chmod 755 %s failed!\n' "/run/screen"
+    exit 4
 fi 
 
 #    sudo rm -fr /var/run/screen/*
 #
 #
 
-SCR_LS="`screen -ls`"
-if [[ $? -eq 0 ]]; then 
-    ${ASC_DIR}/asc yellow blink
-    printf '\nERROR-4: A GNU screen session already exist:\n'
-    ${ASC_DIR}/asc unblink red
-    printf '\n%s\n\n' "${SCR_LS}"
-    ${ASC_DIR}/asc reset
-    exit 4 
-fi 
-
-SCREENRC_FILE=""/etc/screenrc.d/usbcon-screenrc""
+SCREENRC_FILE="/etc/screenrc.d/usbcon-screenrc"
 
 if [[ -f "${SCREENRC_FILE}" ]]; then 
-    screen -c ${SCREENRC_FILE} -d -m -S shared
+    screen -d -m -c "${SCREENRC_FILE}" -S shared
 else
     printf '\nERROR-9: cannot find file: %s\n' "${SCREENRC_FILE}"
     exit 9
